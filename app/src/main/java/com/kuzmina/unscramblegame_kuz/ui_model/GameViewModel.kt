da361f057ee6960082f0ad86d7bb6bfb1b48a8bf
alincqeek <a72658695@gmail.com>
2026-03-10 15:12:33 +0300

    a

--- app/src/main/java/com/kuzmina/unscramblegame_kuz/ui_model/GameViewModel.kt
+++ app/src/main/java/com/kuzmina/unscramblegame_kuz/ui_model/GameViewModel.kt
@@ -1,18 +1,52 @@
 package com.kuzmina.unscramblegame_kuz.ui_model
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.kuzmina.unscramblegame_kuz.data.GameUiState
+import com.kuzmina.unscramblegame_kuz.data.MAX_NO_OF_WORDS
+import com.kuzmina.unscramblegame_kuz.data.SCORE_INCREASE
 import com.kuzmina.unscramblegame_kuz.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel(){
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
+    var userGuess by mutableStateOf("")
+        private set
+    fun updateUserGuess(guessedWord: String) {
+        userGuess = guessedWord
+    }
+    private fun updateGameState(updateScore: Int){
+        if (usedWords.size === MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updateScore,
+                    isGameOver = true
+                )
+            }
+        }
+        else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordShuffle(),
+                    score = updateScore,
+                    currentWordCount = currentState.currentWordCount + 1
+                )
+            }
+        }
+    }
+    fun checkUserGuess(){
+
+    }
     private fun shuffleCurrentWord(word: String): String{
         val tempWord = word.toCharArray()
         tempWord.shuffle()
